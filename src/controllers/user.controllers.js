c67954b5b62833e59f23a1de67f56964ad99e885
waltercp <walter_cp_96@hotmail.com>
2023-06-05 18:24:46 -0600

    user

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -2,40 +2,45 @@ const catchError = require('../utils/catchError');
 const User = require('../models/User');
 
 const getAll = catchError(async (req, res) => {
-    const users = await User.findAll()
-    return res.json(users)
+  const users = await User.findAll()
+  return res.json(users)
 });
 
 const create = catchError(async (req, res) => {
-    const user = req.body
-
-    const createUser = await User.create(user)
-    return res.status(201).json(createUser)
+  const user = req.body
+  const createUser = await User.create(user)
+  return res.status(201).json(createUser)
 })
 
 const getOne = catchError(async (req, res) => {
-    const { id } = req.params
-    const user = await User.findByPk(id)
-    return res.json(user)
+  const { id } = req.params
+  const user = await User.findByPk(id)
+  if (!user) return res.status(404).json({ message: "User not found" })
+  return res.json(user)
 })
 
+
 const remove = catchError(async (req, res) => {
-    const { id } = req.params
-    const removeUser = await User.destroy({where: { id } })
-    return res.sendStatus(204)
+  const { id } = req.params
+  const removeUser = await User.destroy({ where: { id } })
+  if (!removeUser) return res.status(404).json({ message: "User not found" })
+  return res.sendStatus(204)
+
 })
 
 const update = catchError(async (req, res) => {
-    const { id } = req.params
-    const user = req.body
-    const userUpdate = await User.update(user, {where: { id } , returning: true })
-    return res.json(userUpdate[1][0])
+  const { id } = req.params
+  const user = req.body
+
+  const userUpdate = await User.update(user, { where: { id }, returning: true })
+  if (userUpdate[0] === 0) return res.status(404).json({ message: "User not found" })
+  return res.json(userUpdate[1][0])
 })
 
 module.exports = {
-    getAll,
-    create,
-    getOne,
-    remove,
-    update
+  getAll,
+  create,
+  getOne,
+  remove,
+  update
 }
